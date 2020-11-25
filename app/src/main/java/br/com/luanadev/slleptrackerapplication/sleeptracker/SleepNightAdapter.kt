1108a8475cc42367a48efb8dc0ab69ac65093b30
Luana Souza <luana.souza@ifood.com.br>
2020-11-25 12:43:13 -0300

    finish

--- app/src/main/java/br/com/luanadev/slleptrackerapplication/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/br/com/luanadev/slleptrackerapplication/sleeptracker/SleepNightAdapter.kt
@@ -88,12 +88,6 @@ class SleepNightAdapter(val clickListener: SleepNightListener) : ListAdapter<Dat
     }
 }
 
-/**
- * Callback for calculating the diff between two non-null items in a list.
- *
- * Used by ListAdapter to calculate the minumum number of changes between and old list and a new
- * list that's been passed to `submitList`.
- */
 class SleepNightDiffCallback : DiffUtil.ItemCallback<DataItem>() {
     override fun areItemsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
         return oldItem.id == newItem.id
--- app/src/main/java/br/com/luanadev/slleptrackerapplication/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/br/com/luanadev/slleptrackerapplication/sleeptracker/SleepTrackerFragment.kt
@@ -28,18 +28,14 @@ class SleepTrackerFragment : Fragment() {
         )
 
         val application = requireNotNull(this.activity).application
-
         val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
-
         val viewModelFactory = SleepTrackerViewModelFactory(dataSource, application)
-
         val sleepTrackerViewModel =
             ViewModelProvider(
                 this, viewModelFactory
             ).get(SleepTrackerViewModel::class.java)
 
         binding.sleepTrackerViewModel = sleepTrackerViewModel
-
         binding.lifecycleOwner = this
 
         sleepTrackerViewModel.showSnackBarEvent.observe(viewLifecycleOwner, Observer {
